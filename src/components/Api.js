ed4e1ef6372824358b170d127e99ced8a417ea6b
Marcelluki <marcell.rangel29@gmail.com>
2023-12-05 19:32:16 -0800

    added headers in this._headers to shorten code. also created _checkResponse function in Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,127 +1,79 @@
 export default class Api {
-  constructor(options) {
+  constructor(options, headers) {
     this._options = options;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error ${res.status}`);
   }
 
   getInitialCards() {
     return fetch(`${this._options.baseUrl}/cards`, {
-      headers: {
-        authorization: this._options.headers.authorization,
-      },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      // if the server returns an error, reject the promise
-      return Promise.reject(`Error: ${res.status}`);
-    });
+      headers: this._options.headers,
+    }).then(this._checkResponse);
   }
 
   getUserInfo() {
     return fetch(`${this._options.baseUrl}/users/me`, {
-      headers: {
-        authorization: this._options.headers.authorization,
-      },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    });
+      headers: this._options.headers,
+    }).then(this._checkResponse);
   }
+
   updateUserInfo({ name, job }) {
     return fetch(`${this._options.baseUrl}/users/me`, {
       method: "PATCH",
-      headers: {
-        authorization: this._options.headers.authorization,
-        "Content-Type": "application/json",
-      },
+      headers: this._options.headers,
       body: JSON.stringify({
         name: name,
         about: job,
       }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    });
+    }).then(this._checkResponse);
   }
+
   createCard({ name, link }) {
     return fetch(`${this._options.baseUrl}/cards`, {
       method: "POST",
-      headers: {
-        authorization: this._options.headers.authorization,
-        "Content-Type": "application/json",
-      },
+      headers: this._options.headers,
       body: JSON.stringify({
         name,
         link,
       }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    }).then(this._checkResponse);
   }
 
   setUserAvatar(link) {
     return fetch(`${this._options.baseUrl}/users/me/avatar`, {
       method: "PATCH",
-      headers: {
-        authorization: this._options.headers.authorization,
-        "Content-Type": "application/json",
-      },
+      headers: this._options.headers,
       body: JSON.stringify({
         avatar: link,
       }),
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+    }).then(this._checkResponse);
   }
+
   likeCard(cardId) {
     return fetch(`${this._options.baseUrl}/cards/${cardId}/likes`, {
       method: "PUT",
-      headers: {
-        authorization: this._options.headers.authorization,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+      headers: this._options.headers,
+    }).then(this._checkResponse);
   }
+
   dislikeCard(cardId) {
     return fetch(`${this._options.baseUrl}/cards/${cardId}/likes`, {
       method: "DELETE",
-      headers: {
-        authorization: this._options.headers.authorization,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+      headers: this._options.headers,
+    }).then(this._checkResponse);
   }
+
   deleteCard(cardId) {
     return fetch(`${this._options.baseUrl}/cards/${cardId}`, {
       method: "DELETE",
-      headers: {
-        authorization: this._options.headers.authorization,
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Error: ${res.status}`);
-    });
+      headers: this._options.headers,
+    }).then(this._checkResponse);
   }
 }
 
